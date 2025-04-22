6159004a366bb33c4aafbc1e5268e3cc6a1edd00
harsh gupta <hg66146@gmail.com>
2025-04-22 15:40:47 +0530

    done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Hero from './Hero.jsx'
+import BrandsSection from './BrandsSection.jsx'
+import StorySection from './StorySection.jsx'
+import ServicesSection from './ServicesSection.jsx'
+import PortfolioSection from './PortfolioSection.jsx'
+import TestimonialsSection from './TestimonialsSection.jsx'
+import ContactSection from './ContactSection.jsx'
 
+import { Contact } from 'lucide-react'
 const Home = () => {
   return (
     <div>
         <Hero/>
+        <BrandsSection/>
+        <StorySection/>
+        <ServicesSection/>
+        <PortfolioSection/>
+        <TestimonialsSection/>
+        <ContactSection/>
     </div>
   )
 }
