14fb244a038c20181d72870b6b5fddbe1002324e
harsh gupta <hg66146@gmail.com>
2025-05-02 17:40:48 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,7 +52,7 @@ useEffect(() => {
             <li><Link to="/" className="text-white hover:text-cyan-400 text-lg transition-colors">Home</Link></li>
             <li><Link to="/servicepage" className="text-gray-300 hover:text-cyan-400 text-lg transition-colors">Services</Link></li>
             <li><Link to="/portfoliopage" className="text-gray-300 hover:text-cyan-400 text-lg transition-colors">Portfolio</Link></li>
-            <li><Link to="/about" className="text-gray-300 hover:text-cyan-400 text-lg transition-colors">About Us</Link></li>
+            <li><Link to="/about" className="text-gray-300 hover:text-cyan-400 text-lg transition-colors">AboutUs</Link></li>
             <li><Link to="/blog" className="text-gray-300 hover:text-cyan-400 text-lg transition-colors">Blog</Link></li>
             
         {showContactButton && (<li><Link
